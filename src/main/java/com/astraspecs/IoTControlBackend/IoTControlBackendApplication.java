f25203d79cfe36d6f87af0dca5f42c7b64ed84fd
Moafk Aljabi <mwafak_sh@hotmail.com>
2025-04-13 01:36:07 +0200

    Started the backend

---
+++ src/main/java/com/astraspecs/IoTControlBackend/IoTControlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.astraspecs.IoTControlBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTControlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTControlBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astraspecs/IoTControlBackend/IoTControlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astraspecs.IoTControlBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTControlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
